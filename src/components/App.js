acd445bc3bb000be7063ab5172484c4e758a79f8
Albert Agram <albert.agram@gmail.com>
2018-06-28 17:46:52 +0000

    import Home, Header components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Header from './Header';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
       <div>
-        { this.props.appName }
+        <Header appName={this.props.appName} />
+        <Home />
       </div>
     );
   }
