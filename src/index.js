05690f5a671354a2f8d5b83e50eb745703d727c7
Albert Agram <albert.agram@gmail.com>
2018-06-29 11:32:09 +0000

    setup routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import App from './components/App';
 import { Provider } from 'react-redux';
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import store from './store'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from './components/App';
+import Login from './components/Login'
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Fragment>
+        <Route path="/" exact component={App} />
+        <Route path="/login" component={Login} />
+      </Fragment>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
