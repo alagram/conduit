64ea46fb7a04b79d2208b23a18c81e285095ea04
Albert Agram <albert.agram@gmail.com>
2018-07-02 17:09:52 +0000

    implement requests.put

--- src/agent.js
+++ src/agent.js
@@ -11,7 +11,10 @@ const requests = {
   get: url =>
     superagent.get(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
   post: (url, body) =>
-    superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
+    superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody),
+  put: (url, body) => {
+    superagent.put(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
+  }
 };
 
 const Articles = {
