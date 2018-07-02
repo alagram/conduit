8657e40265892f865e7dc262a75898fbd5904140
Albert Agram <albert.agram@gmail.com>
2018-07-02 13:41:43 +0000

    LoggedOutView

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+const LoggedOutView = props => {
+  if (!props.currentUser) {
+    return (
+      <ul className="nav navbar-nav pull-xs-right">
+
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="login" className="nav-link">
+            Sign In
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="register" className="nav-link">
+            Sign Up
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+
+  return null;
+}
+
 class Header extends Component {
   render() {
     return (
