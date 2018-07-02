9dec90c688ab927090e1ec5b9b7093570fc05344
Albert Agram <albert.agram@gmail.com>
2018-07-02 13:54:36 +0000

    destructure props in LoggedOutView

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const LoggedOutView = props => {
-  if (!props.currentUser) {
+const LoggedOutView = ({ currentUser }) => {
+  if (!currentUser) {
     return (
       <ul className="nav navbar-nav pull-xs-right">
 
