e1e57e26b8601efa039889d555b9fc918471b910
Albert Agram <albert.agram@gmail.com>
2018-07-26 19:59:09 +0000

    render Profile component

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Login from './Login';
 import Register from './Register';
 import Settings from './Settings';
 import Article from './Article';
+import Profile from './Profile';
 import agent from '../agent';
 import { store } from '../store';
 import { push } from 'react-router-redux';
@@ -40,6 +41,7 @@ class App extends Component {
           <Route path="/register" component={Register} />
           <Route path="/settings" component={Settings} />
           <Route path="/article/:id" component={Article} />
+          <Route path="/@:username" component={Profile} />
         </Switch>
       </div>
     );
