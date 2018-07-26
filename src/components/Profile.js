89e660af6f54e1ec9bb23e3ab2c7e4a5964845e5
Albert Agram <albert.agram@gmail.com>
2018-07-26 19:08:22 +0000

    FollowUserButton component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -104,6 +104,41 @@ const EditProfileSettings = props => {
 
   return null;
 }
+
+const FollowUserButton = props => {
+  if (props.isUser) {
+    return null;
+  }
+
+  let classes = 'btn btn-sm action-btn';
+
+  if (props.user.following) {
+    classes += ' btn-secondary';
+  } else {
+    classes += ' btn-outline-secondary';
+  }
+
+  const handleClick = event => {
+    event.preventDefault();
+
+    if (props.user.following) {
+      props.unfollow(props.user.username)
+    } else {
+      props.follow(props.user.username)
+    }
+  };
+
+  return (
+    <button
+      className={classes}
+      onClick={handleClick}>
+      <i className="ion-plus-round"></i>
+      &nbsp;
+      {props.user.following ? 'Unfollow' : 'Follow'} {props.user.username}
+    </button>
+  );
+}
+
 const mapStateToProps = state => ({
   ...state.articleList,
   currentUser: state.common.currentUser,
