6acd2094399f0f1b80f93d68ad9403dce464e3f1
Albert Agram <albert.agram@gmail.com>
2018-08-13 22:51:51 +0000

    fix urls

--- src/components/Article/Comment.js
+++ src/components/Article/Comment.js
@@ -12,12 +12,12 @@ const Comment = props => {
         <p className="card-text">{comment.body}</p>
       </div>
       <div className="card-footer">
-        <Link to={`@${comment.author.username}`} className="comment-author">
+        <Link to={`/@${comment.author.username}`} className="comment-author">
           <img src={comment.author.image} className="comment-author-img" alt={comment.author.username} />
         </Link>
 
         &nbsp;
-        <Link to={`${comment.author.username}`} className="comment-author">
+        <Link to={`/@${comment.author.username}`} className="comment-author">
           {comment.author.username}
         </Link>
 
